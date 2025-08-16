a732965ec2a321276b3ca3bcf0a52c8573ca7756
Vadim-Makhnev <vadik.mahnev@yandex.ru>
2025-08-16 14:30:41 +0300

    add: .gitignore file

--- internal/config/imap.go
+++ internal/config/imap.go
@@ -8,15 +8,17 @@ import (
 type IMAPConfig struct {
 	Username string
 	Password string
+	Server   string
 }
 
 func NewIMAPConfig() (*IMAPConfig, error) {
-	if os.Getenv("IMAP_YA_USERNAME") == "" || os.Getenv("IMAP_YA_PASSWORD") == "" {
+	if os.Getenv("IMAP_YA_USERNAME") == "" || os.Getenv("IMAP_YA_PASSWORD") == "" || os.Getenv("IMAP_SERVER") == "" {
 		return nil, errors.New("Ошибка при настройке конфига, некоторые поля отсутствуют")
 	}
 
 	return &IMAPConfig{
 		Username: os.Getenv("IMAP_YA_USERNAME"),
 		Password: os.Getenv("IMAP_YA_PASSWORD"),
+		Server:   os.Getenv("IMAP_SERVER"),
 	}, nil
 }
